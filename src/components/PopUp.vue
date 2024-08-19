3cbb3246c0a4f02bb49a2b704cdc74f2eacaf677
reziferdiansyah <reziferdiansyah23@gmail.com>
2024-08-19 17:09:25 +0700

    add-pop-up-image

---
+++ src/components/PopUp.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <div
+      class="fixed max-z-index top-0 bottom-0 left-0 right-0 px-6 py-3 flex flex-col"
+    >
+      <img class="background" src="/src/assets/images/popUp.jpg" />
+
+      <div class="flex flex-col items-center gap-3 max-z-index">
+        <h4 class="f-12-global mt-2 font-bold text-center text-white my-1">
+          Title
+        </h4>
+        <h3 class="text-white whitespace-nowrap font-bold my-2 text-xl">
+          Name Here
+        </h3>
+        <h3 class="text-white whitespace-nowrap font-bold my-2 text-xl">
+          Date Here
+        </h3>
+        <button
+          class="bg-transparent text-white border-b-2 border-solid border-green-500 rounded-md px-4 py-2 w-4/5"
+          @click="OpenUndangan"
+        >
+          Buka Undangan
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    OpenUndangan() {
+      // use router
+    },
+  },
+};
+</script>
+
+<style scoped>
+img.background {
+  position: absolute;
+  left: 0px;
+  top: 0px;
+  z-index: 99999;
+  width: 100%;
+  height: 100%;
+}
+.max-z-index {
+  justify-content: center;
+  z-index: 100000;
+}
+</style>
--- src/index.vue
+++ src/index.vue
@@ -1,9 +1,14 @@
 <script setup>
 import Audio from "./components/Audio.vue";
+import PopUp from "./components/PopUp.vue";
 </script>
 
 <template>
   <div>
+    <!-- Pop UP Undangan -->
+    <PopUp />
+
+    <!-- Audio -->
     <Audio />
   </div>
 </template>
