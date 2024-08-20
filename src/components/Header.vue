c473079019dafd74467bac47500234ee1681ca4f
reziferdiansyah <reziferdiansyah23@gmail.com>
2024-08-20 08:51:11 +0700

    add-header-and-inform-text-data-dummy

---
+++ src/components/Header.vue
@@ -0,0 +1,29 @@
+<template>
+  <div v-if="DummyData === true" class="dummy-mode">
+    sedang menggunakan data contoh
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      DummyData: true,
+    };
+  },
+};
+</script>
+
+<style scoped>
+.dummy-mode {
+  position: fixed;
+  z-index: 99;
+  background: rgba(0, 0, 0, 0.3);
+  padding: 5px 15px;
+  color: #fff;
+  border-radius: 50px;
+  left: 10px;
+  top: 10px;
+  font-size: 10px;
+}
+</style>
--- src/index.vue
+++ src/index.vue
@@ -1,10 +1,14 @@
 <script setup>
 import Audio from "./components/Audio.vue";
 import PopUp from "./components/PopUp.vue";
+import Header from "./components/Header.vue";
 </script>
 
 <template>
   <div>
+    <!-- ADD HEADER -->
+    <Header />
+
     <!-- Pop UP Undangan -->
     <PopUp />
 
