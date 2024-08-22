7e857ad768adf21292aeb3f01aa754d5419f497d
reziferdiansyah <reziferdiansyah23@gmail.com>
2024-08-22 08:59:37 +0700

    setup-vue3-carousel

--- src/components/ImageSlide.vue
+++ src/components/ImageSlide.vue
@@ -1,3 +1,54 @@
 <template>
-  <div><img alt="bismillah" src="/src/assets/images/popUp.jpg" /></div>
+  <div class="bg-red-500">
+    <!-- LIST MASTER -->
+    <Carousel
+      :autoplay="2000"
+      id="gallery"
+      :items-to-show="1"
+      :wrap-around="false"
+      v-model="currentSlide"
+    >
+      <Slide v-for="slide in 6" :key="slide">
+        <div class="carousel__item">{{ slide }}</div>
+      </Slide>
+    </Carousel>
+
+    <!-- PREVIEW -->
+    <Carousel
+      id="thumbnails"
+      :items-to-show="3"
+      :wrap-around="true"
+      v-model="currentSlide"
+      ref="carousel"
+    >
+      <Slide v-for="slide in 6" :key="slide">
+        <div class="carousel__item" @click="slideTo(slide - 1)">
+          {{ slide }}
+        </div>
+      </Slide>
+    </Carousel>
+  </div>
 </template>
+
+<script>
+import { defineComponent } from "vue";
+import { Carousel, Slide } from "vue3-carousel";
+import "vue3-carousel/dist/carousel.css";
+
+export default defineComponent({
+  name: "ImageSlide",
+  components: {
+    Carousel,
+    Slide,
+    Navigation,
+  },
+  data: () => ({
+    currentSlide: 0,
+  }),
+  methods: {
+    slideTo(val) {
+      this.currentSlide = val;
+    },
+  },
+});
+</script>
