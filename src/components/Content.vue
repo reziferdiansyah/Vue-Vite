bbddca876b6dfb19d2cceca31c6f2071aeb3be01
reziferdiansyah <reziferdiansyah23@gmail.com>
2024-08-27 16:16:33 +0700

    dd-Footer-Section

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -1,44 +1,47 @@
 <template>
-    <div>
-      <!-- Hero Slide Images -->
-      <ImageSlide />
-  
-      <!-- CONTENT -->
-      <div class="text-black gap-5 padding-content">
-        <h3 class="text-heading-title">Kepada Yth.<br />Bapak/Ibu/Saudara/i</h3>
-        <div style="font-size: 16px" class="my-5">Name Invitation</div>
-        <h3 class="text-heading-title">
-          Mohon maaf apabila ada kesalahan<br />penulisan nama dan gelar
-        </h3>
-        <img alt="bismillah" src="/src/assets/images/bismillah-1.png" />
-        <div class="text-heading-title">
-          <p>Assalamualaikum Warahmatullahi Wabarakatuh</p>
-        </div>
-        <div class="text-heading-title">
-          <p>
-            Maha suci Allah SWT yang menciptakan makhluk-Nya berpasang - pasangan,
-            dengan memohon ridho-Nya, InsyaAllah kami akan melangsungkan
-            pernikahan
-          </p>
-        </div>
+  <div>
+    <!-- Hero Slide Images -->
+    <ImageSlide />
+
+    <!-- CONTENT -->
+    <div class="text-black gap-5 padding-content">
+      <h3 class="text-heading-title">Kepada Yth.<br />Bapak/Ibu/Saudara/i</h3>
+      <div style="font-size: 16px" class="my-5">Name Invitation</div>
+      <h3 class="text-heading-title">
+        Mohon maaf apabila ada kesalahan<br />penulisan nama dan gelar
+      </h3>
+      <img alt="bismillah" src="/src/assets/images/bismillah-1.png" />
+      <div class="text-heading-title">
+        <p>Assalamualaikum Warahmatullahi Wabarakatuh</p>
+      </div>
+      <div class="text-heading-title">
+        <p>
+          Maha suci Allah SWT yang menciptakan makhluk-Nya berpasang - pasangan,
+          dengan memohon ridho-Nya, InsyaAllah kami akan melangsungkan
+          pernikahan
+        </p>
       </div>
     </div>
-  </template>
-  
-  <script setup>
-  import ImageSlide from "./ImageSlide.vue";
-  </script>
-  
-  <style scoped>
-  .text-heading-title {
-    font-size: 14px;
-    margin: 20px 0px 20px 0px;
-  }
-  .padding-content {
-    max-width: 1280px;
-    margin: 0 auto;
-    padding: 1rem;
-    text-align: center;
-  }
-  </style>
-  
+
+    <!-- FOOTER -->
+    <Footer />
+  </div>
+</template>
+
+<script setup>
+import ImageSlide from "./ImageSlide.vue";
+import Footer from "./Footer.vue";
+</script>
+
+<style scoped>
+.text-heading-title {
+  font-size: 14px;
+  margin: 20px 0px 20px 0px;
+}
+.padding-content {
+  max-width: 1280px;
+  margin: 0 auto;
+  padding: 1rem;
+  text-align: center;
+}
+</style>
---
+++ src/components/Footer.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="text-black">
+    © 2024 Allrights reserved. Developed by Rezi Ferdiansyah
+  </div>
+</template>
